8ae9c8bc83b4358f960225275f4b55bfb479a4df
hclee <dlgockd45@gmail.com>
2019-11-24 04:59:53 +0900

    Modify navigation bar to place under bottom sheet

--- app/src/main/java/com/mashup/telltostar/ui/main/MainActivity.kt
+++ app/src/main/java/com/mashup/telltostar/ui/main/MainActivity.kt
@@ -4,7 +4,6 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.Gravity
 import android.view.MenuItem
-import android.view.WindowManager
 import com.mashup.telltostar.R
 import kotlinx.android.synthetic.main.activity_main.*
 import kotlinx.android.synthetic.main.main_contents.*
@@ -12,41 +11,16 @@ import timber.log.Timber
 
 class MainActivity : AppCompatActivity() {
 
-    private val statusHeight by lazy {
-        resources.getDimensionPixelSize(
-            resources.getIdentifier(
-                "status_bar_height",
-                "dimen",
-                "android"
-            )
-        )
-    }
-
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
 
         setContentView(R.layout.activity_main)
         initLayout()
-        initBottomSheet()
 
     }
 
     private fun initLayout() {
-        with(appBarLayout) {
-            setPadding(
-                paddingLeft,
-                statusHeight,
-                paddingRight,
-                paddingBottom
-            )
-        }
-
         setSupportActionBar(toolbar)
-
-        window.setFlags(
-            WindowManager.LayoutParams.FLAG_LAYOUT_NO_LIMITS,
-            WindowManager.LayoutParams.FLAG_LAYOUT_NO_LIMITS
-        )
 
         supportActionBar?.let {
             Timber.d("action bar")
@@ -55,17 +29,6 @@ class MainActivity : AppCompatActivity() {
             it.setDisplayShowTitleEnabled(false)
             it.setDisplayHomeAsUpEnabled(true)
             it.setHomeAsUpIndicator(R.drawable.ic_menu_white_24dp)
-        }
-    }
-
-    private fun initBottomSheet() {
-        with(cdlMainContentsBottomSheetParent) {
-            setPadding(
-                paddingLeft,
-                statusHeight,
-                paddingRight,
-                paddingBottom
-            )
         }
     }
 
