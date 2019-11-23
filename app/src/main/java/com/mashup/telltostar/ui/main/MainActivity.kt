a3e492ae87b2cef9f0f51b1707fe3d174a3532cf
hclee <dlgockd45@gmail.com>
2019-11-24 02:38:28 +0900

    Toolbar의 paddingTop을 status bar의 높이 만큼으로 변경

--- app/src/main/java/com/mashup/telltostar/ui/main/MainActivity.kt
+++ app/src/main/java/com/mashup/telltostar/ui/main/MainActivity.kt
@@ -8,26 +8,47 @@ import android.view.WindowManager
 import com.mashup.telltostar.R
 import kotlinx.android.synthetic.main.activity_main.*
 import kotlinx.android.synthetic.main.main_contents.*
+import timber.log.Timber
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-        setSupportActionBar(toolbar)
         initLayout()
+
+
     }
 
     private fun initLayout() {
+        with(appbar_layout) {
+            setPadding(
+                paddingLeft,
+                resources.getDimensionPixelSize(
+                    resources.getIdentifier(
+                        "status_bar_height",
+                        "dimen",
+                        "android"
+                    )
+                ),
+                paddingRight,
+                paddingBottom
+            )
+        }
+
+        setSupportActionBar(toolbar)
+
         window.setFlags(
             WindowManager.LayoutParams.FLAG_LAYOUT_NO_LIMITS,
             WindowManager.LayoutParams.FLAG_LAYOUT_NO_LIMITS
         )
 
         supportActionBar?.let {
+            Timber.d("action bar")
+
             it.setDisplayShowCustomEnabled(true)
             it.setDisplayShowTitleEnabled(false)
             it.setDisplayHomeAsUpEnabled(true)
-            it.setHomeAsUpIndicator(R.drawable.ic_menu_black_24dp)
+            it.setHomeAsUpIndicator(R.drawable.ic_menu_white_24dp)
         }
     }
 
