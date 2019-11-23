0361bba3de5cd80f031d884c7bb4f085dd82f5c7
miinjung <laura126@ajou.ac.kr>
2019-11-24 03:39:52 +0900

    modify bottomSheet fragment

--- app/src/main/java/com/mashup/telltostar/ui/main/MainActivity.kt
+++ app/src/main/java/com/mashup/telltostar/ui/main/MainActivity.kt
@@ -12,7 +12,7 @@ class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        setContentView(R.layout.fragment_bottomsheet)
 
         with(mTintManager) {
             isStatusBarTintEnabled = true
