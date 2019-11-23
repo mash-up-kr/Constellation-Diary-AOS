4df598e8fc9926f10a3160395508240305e5347e
hclee <dlgockd45@gmail.com>
2019-11-24 05:46:32 +0900

    Modify drawer not to open if bottom sheet expanded

--- app/src/main/java/com/mashup/telltostar/ui/main/MainActivity.kt
+++ app/src/main/java/com/mashup/telltostar/ui/main/MainActivity.kt
@@ -4,8 +4,12 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.Gravity
 import android.view.MenuItem
+import android.view.View
+import androidx.drawerlayout.widget.DrawerLayout
+import com.google.android.material.bottomsheet.BottomSheetBehavior
 import com.mashup.telltostar.R
 import kotlinx.android.synthetic.main.activity_main.*
+import kotlinx.android.synthetic.main.fragment_bottomsheet.*
 import kotlinx.android.synthetic.main.main_contents.*
 import timber.log.Timber
 
@@ -30,6 +34,27 @@ class MainActivity : AppCompatActivity() {
             it.setDisplayHomeAsUpEnabled(true)
             it.setHomeAsUpIndicator(R.drawable.ic_menu_white_24dp)
         }
+
+        BottomSheetBehavior.from(llBottomSheetView)
+            .addBottomSheetCallback(object : BottomSheetBehavior.BottomSheetCallback() {
+                override fun onSlide(bottomSheet: View, slideOffset: Float) {
+
+                }
+
+                override fun onStateChanged(bottomSheet: View, newState: Int) {
+                    when (newState) {
+                        BottomSheetBehavior.STATE_EXPANDED -> {
+                            drawer_container.setDrawerLockMode(DrawerLayout.LOCK_MODE_LOCKED_CLOSED)
+                        }
+                        BottomSheetBehavior.STATE_COLLAPSED -> {
+                            drawer_container.setDrawerLockMode(DrawerLayout.LOCK_MODE_UNLOCKED)
+                        }
+                        else -> {
+
+                        }
+                    }
+                }
+            })
     }
 
     override fun onOptionsItemSelected(item: MenuItem?): Boolean {
