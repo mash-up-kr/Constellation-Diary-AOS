c7284408237bc2ac1cda1c6f23ca40c04c4f6bdf
hclee <dlgockd45@gmail.com>
2019-11-23 22:14:28 +0900

    메인화면 네비게이션 드로워 추가

--- app/src/main/java/com/mashup/telltostar/ui/main/MainActivity.kt
+++ app/src/main/java/com/mashup/telltostar/ui/main/MainActivity.kt
@@ -3,11 +3,22 @@ package com.mashup.telltostar.ui.main
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import com.mashup.telltostar.R
+import com.readystatesoftware.systembartint.SystemBarTintManager
 
 class MainActivity : AppCompatActivity() {
+    private val mTintManager: SystemBarTintManager by lazy {
+        SystemBarTintManager(this@MainActivity)
+    }
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        with(mTintManager) {
+            isStatusBarTintEnabled = true
+            setNavigationBarTintEnabled(true)
+            setTintColor(android.R.color.holo_green_dark)
+//            setNavigationBarTintResource()
+        }
     }
 }
