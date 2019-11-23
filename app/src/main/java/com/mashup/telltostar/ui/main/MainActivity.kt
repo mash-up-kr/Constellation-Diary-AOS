2bbe5fba8e01dbda8a076138ecffc6cf74b64409
hclee <dlgockd45@gmail.com>
2019-11-24 00:43:17 +0900

    툴바에 햄버거 버튼 추가

--- app/src/main/java/com/mashup/telltostar/ui/main/MainActivity.kt
+++ app/src/main/java/com/mashup/telltostar/ui/main/MainActivity.kt
@@ -2,9 +2,14 @@ package com.mashup.telltostar.ui.main
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.Gravity
+import android.view.MenuItem
 import android.view.WindowManager
 import com.mashup.telltostar.R
 import com.readystatesoftware.systembartint.SystemBarTintManager
+import kotlinx.android.synthetic.main.activity_main.*
+import kotlinx.android.synthetic.main.main_contents.*
+import timber.log.Timber
 
 class MainActivity : AppCompatActivity() {
     private val mTintManager: SystemBarTintManager by lazy {
@@ -14,17 +19,36 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        setSupportActionBar(toolbar)
 
         with(mTintManager) {
             isStatusBarTintEnabled = true
             setNavigationBarTintEnabled(true)
             setTintColor(android.R.color.holo_green_dark)
-//            setNavigationBarTintResource()
         }
 
         window.setFlags(
             WindowManager.LayoutParams.FLAG_LAYOUT_NO_LIMITS,
             WindowManager.LayoutParams.FLAG_LAYOUT_NO_LIMITS
         )
+
+        supportActionBar?.let {
+            it.setDisplayShowCustomEnabled(true)
+            it.setDisplayShowTitleEnabled(false)
+            it.setDisplayHomeAsUpEnabled(true)
+            it.setHomeAsUpIndicator(R.drawable.ic_menu_black_24dp)
+        }
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem?): Boolean {
+        when (item?.itemId) {
+            android.R.id.home -> {
+                Timber.d("item select")
+
+                drawer_container.openDrawer(Gravity.LEFT)
+            }
+        }
+
+        return super.onOptionsItemSelected(item)
     }
 }
