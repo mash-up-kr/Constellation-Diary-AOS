3f4b4744db4d19f2dc90391d0ba49a04c088c4d4
hclee <dlgockd45@gmail.com>
2019-11-26 01:11:55 +0900

    Add DiaryEditActivity

---
+++ app/src/main/java/com/mashup/telltostar/ui/diary/DiaryEditActivity.kt
@@ -0,0 +1,20 @@
+package com.mashup.telltostar.ui.diary
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.view.View
+import com.mashup.telltostar.R
+
+class DiaryEditActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+        setContentView(R.layout.activity_diary_edit)
+
+    }
+
+    fun onClick(view: View) {
+
+    }
+}
