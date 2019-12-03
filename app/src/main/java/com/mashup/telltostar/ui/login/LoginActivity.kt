441e5369ef12d736ff3fe8f1fde64f56aa1e7b87
hclee <dlgockd45@gmail.com>
2019-12-04 00:31:59 +0900

    Add toolbar into LoginActivity

--- app/src/main/java/com/mashup/telltostar/ui/login/LoginActivity.kt
+++ app/src/main/java/com/mashup/telltostar/ui/login/LoginActivity.kt
@@ -3,6 +3,7 @@ package com.mashup.telltostar.ui.login
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import com.mashup.telltostar.R
+import kotlinx.android.synthetic.main.activity_login.*
 
 class LoginActivity : AppCompatActivity() {
 
@@ -10,5 +11,17 @@ class LoginActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_login)
 
+        initToolbar()
+    }
+
+    private fun initToolbar() {
+        setSupportActionBar(toolbar)
+
+        supportActionBar?.let {
+            it.setDisplayHomeAsUpEnabled(true)
+            it.setDisplayShowTitleEnabled(false)
+            it.setDisplayHomeAsUpEnabled(true)
+            it.setHomeAsUpIndicator(R.drawable.ic_menu_white_24dp)
+        }
     }
 }
