528d746cc8448b4713fc2bfa08ba586907b5cf4d
dlgockd54 <dlgockd45@gmail.com>
2019-12-05 11:04:30 +0900

    Add MyConstellationActivity

---
+++ app/src/main/java/com/mashup/telltostar/ui/myconstellation/MyConstellationActivity.kt
@@ -0,0 +1,15 @@
+package com.mashup.telltostar.ui.myconstellation
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.mashup.telltostar.R
+import kotlinx.android.synthetic.main.activity_my_constellation.*
+
+class MyConstellationActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_my_constellation)
+
+    }
+}
