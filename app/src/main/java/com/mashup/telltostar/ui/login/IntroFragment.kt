728ab6ce73f82ccd16a72c8c0131f8d24a3c84c8
dlgockd54 <dlgockd45@gmail.com>
2019-12-06 14:25:04 +0900

    Modify LoginActivity to fragment replaceable

---
+++ app/src/main/java/com/mashup/telltostar/ui/login/IntroFragment.kt
@@ -0,0 +1,20 @@
+package com.mashup.telltostar.ui.login
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+
+import com.mashup.telltostar.R
+
+class IntroFragment : Fragment() {
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_intro, container, false)
+    }
+}
--- app/src/main/java/com/mashup/telltostar/ui/login/LoginActivity.kt
+++ app/src/main/java/com/mashup/telltostar/ui/login/LoginActivity.kt
@@ -2,16 +2,22 @@ package com.mashup.telltostar.ui.login
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import androidx.fragment.app.Fragment
 import com.mashup.telltostar.R
 import kotlinx.android.synthetic.main.activity_login.*
 
 class LoginActivity : AppCompatActivity() {
+    private val mIntroFragment by lazy {
+        IntroFragment()
+    }
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_login)
 
         initToolbar()
+        replaceFragment(mIntroFragment)
     }
 
     private fun initToolbar() {
@@ -21,5 +27,9 @@ class LoginActivity : AppCompatActivity() {
             it.setDisplayHomeAsUpEnabled(true)
             it.setDisplayShowTitleEnabled(false)
         }
+    }
+
+    private fun replaceFragment(fragment: Fragment) {
+        supportFragmentManager.beginTransaction().replace(loginFrame.id, fragment).commit()
     }
 }
