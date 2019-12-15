cb2170c16fa38c65165055adfa32871c37aab568
hclee <dlgockd45@gmail.com>
2019-12-16 01:13:22 +0900

    Add bottom sheet into LoginActivity

--- app/src/main/java/com/mashup/telltostar/ui/login/LoginActivity.kt
+++ app/src/main/java/com/mashup/telltostar/ui/login/LoginActivity.kt
@@ -1,8 +1,13 @@
 package com.mashup.telltostar.ui.login
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
 import com.mashup.telltostar.R
+import com.mashup.telltostar.ui.main.MainActivity
+import kotlinx.android.synthetic.main.activity_login.*
+import kotlinx.android.synthetic.main.bottom_sheet_login.*
 
 class LoginActivity : AppCompatActivity() {
 
@@ -10,5 +15,19 @@ class LoginActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_login)
 
+    }
+
+    fun onClick(view: View) {
+        when (view) {
+            loginSingUpButton -> {
+                login_sign_up_bottom_sheet.visibility = View.VISIBLE
+            }
+            startStarStarDiaryButton -> {
+                startActivity(Intent(this@LoginActivity, MainActivity::class.java).apply {
+                    addFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP)
+                })
+                finish()
+            }
+        }
     }
 }
