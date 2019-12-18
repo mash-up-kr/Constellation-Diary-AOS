846536b588d9632435de7cdca643bb553a8147a2
hclee <dlgockd45@gmail.com>
2019-12-18 20:50:26 +0900

    Modify login bottom sheet to contain Fragment

--- app/src/main/java/com/mashup/telltostar/ui/login/LoginActivity.kt
+++ app/src/main/java/com/mashup/telltostar/ui/login/LoginActivity.kt
@@ -1,18 +1,23 @@
 package com.mashup.telltostar.ui.login
 
-import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.View
+import androidx.fragment.app.Fragment
 import com.google.android.material.bottomsheet.BottomSheetBehavior
 import com.mashup.telltostar.R
-import com.mashup.telltostar.ui.myconstellation.MyConstellationActivity
 import kotlinx.android.synthetic.main.activity_login.*
-import kotlinx.android.synthetic.main.bottom_sheet_login.*
+import kotlinx.android.synthetic.main.fragment_login.*
 
 class LoginActivity : AppCompatActivity() {
     private val mBottomSheetBehavior by lazy {
         BottomSheetBehavior.from(login_sign_up_bottom_sheet)
+    }
+    private val mLoginFragment by lazy {
+        LoginFragment()
+    }
+    private val mSignUpFragment by lazy {
+        SignUpFragment()
     }
 
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -34,29 +39,43 @@ class LoginActivity : AppCompatActivity() {
 
             override fun onStateChanged(bottomSheet: View, newState: Int) {}
         })
+
+        replaceBottomSheetFragment(mLoginFragment)
+    }
+
+    private fun replaceBottomSheetFragment(
+        fragment: Fragment,
+        enterAnim: Int = 0,
+        exitAnim: Int = 0
+    ) {
+        supportFragmentManager
+            .beginTransaction()
+            .setCustomAnimations(enterAnim, exitAnim)
+            .replace(R.id.bottomSheetContainer, fragment)
+            .commit()
     }
 
     fun onClick(view: View) {
         when (view) {
+            closeImageView -> {
+                mBottomSheetBehavior.state = BottomSheetBehavior.STATE_HIDDEN
+                setDimLayoutVisibility(View.GONE)
+            }
+            signupTextView -> {
+                replaceBottomSheetFragment(
+                    mSignUpFragment,
+                    R.anim.enter_from_right,
+                    R.anim.exit_to_left
+                )
+            }
+            forgotIdTextView -> {
+
+            }
             loginSingUpButton -> {
                 setDimLayoutVisibility(View.VISIBLE)
 
                 login_sign_up_bottom_sheet.visibility = View.VISIBLE
                 mBottomSheetBehavior.state = BottomSheetBehavior.STATE_COLLAPSED
-            }
-            signupTextView -> {
-
-            }
-            forgotIdTextView -> {
-                
-            }
-            startStarStarDiaryButton -> {
-                startActivity(Intent(
-                    this@LoginActivity, MyConstellationActivity::class.java
-                ).apply {
-                    addFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP)
-                })
-                finish()
             }
         }
     }
