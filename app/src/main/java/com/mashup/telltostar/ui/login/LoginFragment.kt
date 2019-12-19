ff80008cb27bdce055d728e2c67b265ffb3f9d87
hclee <dlgockd45@gmail.com>
2019-12-19 20:52:20 +0900

    Add ID, password empty warning into LoginFragment

--- app/src/main/java/com/mashup/telltostar/ui/login/LoginFragment.kt
+++ app/src/main/java/com/mashup/telltostar/ui/login/LoginFragment.kt
@@ -1,5 +1,7 @@
 package com.mashup.telltostar.ui.login
 
+import android.content.res.ColorStateList
+import android.os.Build
 import android.os.Bundle
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
@@ -7,6 +9,7 @@ import android.view.View
 import android.view.ViewGroup
 
 import com.mashup.telltostar.R
+import kotlinx.android.synthetic.main.fragment_login.*
 import kotlinx.android.synthetic.main.fragment_login.view.*
 
 class LoginFragment : Fragment() {
@@ -46,6 +49,35 @@ class LoginFragment : Fragment() {
             rootView.closeImageView.setOnClickListener {
                 mFragmentListener.closeBottomSheet()
             }
+            rootView.startStarStarDiaryButton.setOnClickListener {
+                performStartButtonClick()
+            }
+        }
+    }
+
+    private fun performStartButtonClick() {
+        if (idEditText.text.isNullOrEmpty()) {
+            idEditText.backgroundTintList =
+                if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M)
+                    ColorStateList.valueOf(resources.getColor(R.color.coral, null))
+                else
+                    ColorStateList.valueOf(resources.getColor(R.color.coral))
+            inputIdWarningTextView.visibility = View.VISIBLE
+        } else {
+            idEditText.backgroundTintList = null
+            inputIdWarningTextView.visibility = View.GONE
+        }
+
+        if (passwordEditText.text.isNullOrEmpty()) {
+            passwordEditText.backgroundTintList =
+                if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M)
+                    ColorStateList.valueOf(resources.getColor(R.color.coral, null))
+                else
+                    ColorStateList.valueOf(resources.getColor(R.color.coral))
+            inputPasswordWarningTextView.visibility = View.VISIBLE
+        } else {
+            passwordEditText.backgroundTintList = null
+            inputPasswordWarningTextView.visibility = View.GONE
         }
     }
 }
