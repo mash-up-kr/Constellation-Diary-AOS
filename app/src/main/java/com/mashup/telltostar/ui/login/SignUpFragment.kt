ce6b288e7d38de7dfabb96cfbc0e30b79e5f57b7
hclee <dlgockd45@gmail.com>
2019-12-19 21:07:44 +0900

    Add EditText empty warning into SignUpFragment

--- app/src/main/java/com/mashup/telltostar/ui/login/SignUpFragment.kt
+++ app/src/main/java/com/mashup/telltostar/ui/login/SignUpFragment.kt
@@ -1,6 +1,8 @@
 package com.mashup.telltostar.ui.login
 
 
+import android.content.res.ColorStateList
+import android.os.Build
 import android.os.Bundle
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
@@ -8,6 +10,7 @@ import android.view.View
 import android.view.ViewGroup
 
 import com.mashup.telltostar.R
+import kotlinx.android.synthetic.main.fragment_signup.*
 import kotlinx.android.synthetic.main.fragment_signup.view.*
 
 class SignUpFragment : Fragment() {
@@ -37,6 +40,47 @@ class SignUpFragment : Fragment() {
                     R.anim.exit_to_right
                 )
             }
+            rootView.loginSignUpButton.setOnClickListener {
+                performSignUpButtonClick()
+            }
+        }
+    }
+
+    private fun performSignUpButtonClick() {
+        if (idEditText.text.isNullOrEmpty()) {
+            idEditText.backgroundTintList =
+                if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M)
+                    ColorStateList.valueOf(resources.getColor(R.color.coral, null))
+                else
+                    ColorStateList.valueOf(resources.getColor(R.color.coral))
+            inputIdWarningTextView.visibility = View.VISIBLE
+        } else {
+            idEditText.backgroundTintList = null
+            inputIdWarningTextView.visibility = View.GONE
+        }
+
+        if (passwordEditText.text.isNullOrEmpty()) {
+            passwordEditText.backgroundTintList =
+                if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M)
+                    ColorStateList.valueOf(resources.getColor(R.color.coral, null))
+                else
+                    ColorStateList.valueOf(resources.getColor(R.color.coral))
+            inputPasswordWarningTextView.visibility = View.VISIBLE
+        } else {
+            passwordEditText.backgroundTintList = null
+            inputPasswordWarningTextView.visibility = View.GONE
+        }
+
+        if (passwordConfirmEditText.text.isNullOrEmpty()) {
+            passwordConfirmEditText.backgroundTintList =
+                if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M)
+                    ColorStateList.valueOf(resources.getColor(R.color.coral, null))
+                else
+                    ColorStateList.valueOf(resources.getColor(R.color.coral))
+            inputPasswordConfirmWarningTextView.visibility = View.VISIBLE
+        } else {
+            passwordConfirmEditText.backgroundTintList = null
+            inputPasswordConfirmWarningTextView.visibility = View.GONE
         }
     }
 }
