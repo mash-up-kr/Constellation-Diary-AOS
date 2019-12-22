b5a044e7b34fe4cbee1424656996132d2f59b5e3
leejinseong <dlwls5201@naver.com>
2019-12-22 22:37:39 +0900

    divide enum type for diary edit

--- app/src/main/java/com/mashup/telltostar/ui/diary/DiaryEditActivity.kt
+++ app/src/main/java/com/mashup/telltostar/ui/diary/DiaryEditActivity.kt
@@ -1,20 +1,37 @@
 package com.mashup.telltostar.ui.diary
 
-import androidx.appcompat.app.AppCompatActivity
+import android.content.Context
+import android.content.Intent
 import android.os.Bundle
-import android.view.View
+import androidx.appcompat.app.AppCompatActivity
 import com.mashup.telltostar.R
+import timber.log.Timber
 
 class DiaryEditActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-
         setContentView(R.layout.activity_diary_edit)
+
+        val type = intent.getSerializableExtra(EXTRA_TYPE) as EditType
+        Timber.d("type : $type")
 
     }
 
-    fun onClick(view: View) {
+    enum class EditType {
+        NEW, MODIFY
+    }
 
+    companion object {
+
+        const val EXTRA_TYPE = "type"
+
+        fun startDiaryEditActivity(context: Context, type: EditType) {
+            context.startActivity(
+                Intent(context, DiaryEditActivity::class.java).apply {
+                    putExtra(EXTRA_TYPE, type)
+                }
+            )
+        }
     }
 }
