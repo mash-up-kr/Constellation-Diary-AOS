a117ba17297cc0995862bdbfb4c88a0dcf6482df
miinjung <laura126@ajou.ac.kr>
2020-01-02 03:33:53 +0900

    make ui

---
+++ app/src/main/java/com/mashup/telltostar/ui/diarylist/DiaryListActivity.kt
@@ -0,0 +1,12 @@
+package com.mashup.telltostar.ui.diarylist
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class DiaryListActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_diary_list)
+    }
+}
---
+++ app/src/main/java/com/mashup/telltostar/ui/diarylist/DiaryListAdapter.kt
@@ -0,0 +1,4 @@
+package com.mashup.telltostar.ui.diarylist
+
+class DiaryListAdapter {
+}
