2ee5c7d974eea6efbcdfa5d4edd1074c83017ac5
miinjung <laura126@ajou.ac.kr>
2020-01-12 00:16:09 +0900

    save temp for fetch

--- app/src/main/java/com/mashup/telltostar/ui/diarylist/DiaryListActivity.kt
+++ app/src/main/java/com/mashup/telltostar/ui/diarylist/DiaryListActivity.kt
@@ -1,12 +1,37 @@
 package com.mashup.telltostar.ui.diarylist
 
+import android.content.Context
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.recyclerview.widget.DividerItemDecoration
+import androidx.recyclerview.widget.LinearLayoutManager
+import com.mashup.telltostar.R
+import com.mashup.telltostar.data.Diary
+import kotlinx.android.synthetic.main.activity_diary_list.*
 
 class DiaryListActivity : AppCompatActivity() {
+    private val diaryListAdapter = DiaryListAdapter()
+    private var diaryList : ArrayList<Diary> = arrayListOf()
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_diary_list)
+
+        initRecycleview()
+    }
+
+    fun initRecycleview(){
+        listDiaryRV.adapter = this.diaryListAdapter
+        listDiaryRV.layoutManager = LinearLayoutManager(this, LinearLayoutManager.VERTICAL, false)
+
+        var diveder : DividerItemDecoration = DividerItemDecoration(this,LinearLayoutManager.VERTICAL)
+        diveder.setDrawable(this.getDrawable(R.drawable.divider_diary_list))
+        listDiaryRV.addItemDecoration(diveder)
+
+        setData()
+    }
+
+    fun setData(){
+        diaryListAdapter.setItem(diaryList)
     }
 }
--- app/src/main/java/com/mashup/telltostar/ui/diarylist/DiaryListAdapter.kt
+++ app/src/main/java/com/mashup/telltostar/ui/diarylist/DiaryListAdapter.kt
@@ -1,4 +1,48 @@
 package com.mashup.telltostar.ui.diarylist
 
-class DiaryListAdapter {
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.TextView
+import androidx.recyclerview.widget.RecyclerView
+import com.mashup.telltostar.R
+import com.mashup.telltostar.data.Diary
+
+class DiaryListAdapter: RecyclerView.Adapter<DiaryListAdapter.DiaryListViewHolder>() {
+    private var diaryList :ArrayList<Diary> = arrayListOf()
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): DiaryListViewHolder {
+        val convertView = LayoutInflater.from(parent.context).inflate(R.layout.fragment_diary_list, parent, false)
+        return DiaryListViewHolder(convertView)
+    }
+
+    override fun getItemCount(): Int = diaryList.size + 1
+
+    override fun onBindViewHolder(holder: DiaryListViewHolder, position: Int) {
+        diaryList[position].let{item ->
+            holder.let{
+                it.diaryContent.text = item.contents
+                it.diaryDate.text = item.date.toString()
+                it.diaryDay.text = item.date.toString()
+                it.diaryTitle.text = item.title
+            }
+        }
+    }
+
+    fun setItem(item:ArrayList<Diary>){
+        diaryList.addAll(item)
+    }
+
+    class DiaryListViewHolder(view: View):RecyclerView.ViewHolder(view){
+        val diaryDate :TextView = view.findViewById(R.id.diaryListDateTV) as TextView
+        val diaryDay : TextView = view.findViewById(R.id.diaryListDayTV) as TextView
+        val diaryTitle : TextView = view.findViewById(R.id.diaryListTitleTV) as TextView
+        val diaryContent : TextView = view.findViewById(R.id.diaryListContentTV) as TextView
+
+    }
+
+    companion object{
+        private val TYPE_HEADER = 0
+        private  val TYPR_ITEM = 1
+    }
 }
