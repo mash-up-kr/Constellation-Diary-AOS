3fea21d4fc00e4865a8bb3ab808d24a5e7271e02
leejinseong <dlwls5201@naver.com>
2020-01-24 11:17:00 +0900

    set date format

--- app/src/main/java/com/mashup/telltostar/util/TimeUtil.kt
+++ app/src/main/java/com/mashup/telltostar/util/TimeUtil.kt
@@ -16,10 +16,15 @@ object TimeUtil {
         return millis / 1000
     }
 
+    private val date = Date()
+
+    fun getDate(): String {
+        //val time = TimeZone.getDefault()
+        //TODO 2020-1-22T15:45:58.222Z
+        return SimpleDateFormat("yyyy-MM-dd'T'HH:mm:ss'.0Z'").format(date)
+    }
+
     fun get(unixTime: Long) =
         SimpleDateFormat("yyyy년 MM월 dd일 a hh시 mm분 ss초").format(Date(unixTime * 1000))
-
-    fun getDateInteger(unixTime: Long) =
-        SimpleDateFormat("yyyyMMdd").format(Date(unixTime * 1000)).toInt()
 
 }
