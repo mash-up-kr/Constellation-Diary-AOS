1d10b51b60261e95e19d2e58de8ac26cc33db42e
hclee <dlgockd45@gmail.com>
2020-01-27 00:06:38 +0900

    Implement ResetPasswordFragment

--- app/src/main/java/com/mashup/telltostar/ui/login/ResetPasswordFragment.kt
+++ app/src/main/java/com/mashup/telltostar/ui/login/ResetPasswordFragment.kt
@@ -6,20 +6,44 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.core.widget.addTextChangedListener
 
 import com.mashup.telltostar.R
+import kotlinx.android.synthetic.main.fragment_reset_password.view.*
 
 class ResetPasswordFragment : Fragment() {
     private lateinit var mFragmentListener: LoginActivity.FragmentListener
+    private lateinit var mRootView: View
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        return inflater.inflate(R.layout.fragment_reset_password, container, false)
+        mRootView = inflater.inflate(R.layout.fragment_reset_password, container, false)
+
+        setListeners()
+
+        return mRootView
     }
 
     fun setFragmentListener(listener: LoginActivity.FragmentListener) {
         mFragmentListener = listener
+    }
+
+    private fun setListeners() {
+        with(mRootView) {
+            newPasswordEditText.addTextChangedListener {
+                it?.let {
+                    loginButton.isEnabled =
+                        it.isNotEmpty() && passwordConfirmEditText.text.isNotEmpty()
+                }
+            }
+            passwordConfirmEditText.addTextChangedListener {
+                it?.let {
+                    loginButton.isEnabled =
+                        it.isNotEmpty() && newPasswordEditText.text.isNotEmpty()
+                }
+            }
+        }
     }
 }
