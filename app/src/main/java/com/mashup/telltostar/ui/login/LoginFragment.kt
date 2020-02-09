10f64da06134e5f9576f206b43f47c3b98b196ae
hclee <dlgockd45@gmail.com>
2020-02-10 01:28:31 +0900

    Hide keyboard as start star star button is clicked

--- app/src/main/java/com/mashup/telltostar/ui/login/LoginFragment.kt
+++ app/src/main/java/com/mashup/telltostar/ui/login/LoginFragment.kt
@@ -1,5 +1,6 @@
 package com.mashup.telltostar.ui.login
 
+import android.content.Context
 import android.content.Intent
 import android.content.res.ColorStateList
 import android.os.Bundle
@@ -7,6 +8,7 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.view.inputmethod.InputMethodManager
 import androidx.appcompat.app.AlertDialog
 import androidx.core.content.ContextCompat
 import androidx.lifecycle.Observer
@@ -146,6 +148,12 @@ class LoginFragment : Fragment() {
                 mRootView.idEditText.text.toString(),
                 mRootView.passwordEditText.text.toString()
             )
+
+            (activity?.getSystemService(Context.INPUT_METHOD_SERVICE) as InputMethodManager)
+                .hideSoftInputFromWindow(
+                    mRootView.passwordEditText.windowToken,
+                    0
+                )
         }
     }
 
