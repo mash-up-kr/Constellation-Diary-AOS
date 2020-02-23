2ccc85ceafd716c3b3d4850f2422b7551af245d2
hclee <dlgockd45@gmail.com>
2020-02-24 00:10:28 +0900

    Count 3 seconds before start login or main

--- app/src/main/java/com/mashup/telltostar/ui/splash/SplashActivity.kt
+++ app/src/main/java/com/mashup/telltostar/ui/splash/SplashActivity.kt
@@ -7,35 +7,79 @@ import androidx.lifecycle.Observer
 import com.mashup.telltostar.R
 import com.mashup.telltostar.ui.login.LoginActivity
 import com.mashup.telltostar.ui.main.MainActivity
-import io.reactivex.Single
+import io.reactivex.Observable
 import io.reactivex.android.schedulers.AndroidSchedulers
-import io.reactivex.disposables.Disposable
+import io.reactivex.disposables.CompositeDisposable
 import io.reactivex.schedulers.Schedulers
+import java.util.concurrent.TimeUnit
 
 class SplashActivity : AppCompatActivity() {
-    private lateinit var mDisposable: Disposable
+    companion object {
+        private const val TIME_LIMIT = 2L
+    }
+
+    private val mCompositeDisposable = CompositeDisposable()
+    private var isTimeCountFinish = false
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_splash)
         setViewModelObserver()
+    }
+
+    override fun onResume() {
+        super.onResume()
+
+        startCount()
         TokenCheckViewModel.requestAuthenticationTokenAvailability()
     }
 
     private fun setViewModelObserver() {
         with(TokenCheckViewModel) {
             shouldStartLogin.observe(this@SplashActivity, Observer {
-                if (it) {
+                if (shouldStartLoginActivity()) {
                     startLoginActivity()
                 }
             })
             shouldStartMain.observe(this@SplashActivity, Observer {
-                if (it) {
+                if (shouldStartMainActivity()) {
                     startMainActivity()
                 }
             })
         }
     }
+
+    private fun startCount() {
+        mCompositeDisposable.add(
+            Observable.interval(1000, 1000, TimeUnit.MILLISECONDS)
+                .take(TIME_LIMIT)
+                .subscribeOn(Schedulers.io())
+                .observeOn(AndroidSchedulers.mainThread())
+                .subscribe({
+                    if (it == (TIME_LIMIT - 1)) {
+                        isTimeCountFinish = true
+                    }
+
+                    if (shouldStartLoginActivity()) {
+                        startLoginActivity()
+                    } else if (shouldStartMainActivity()) {
+                        startMainActivity()
+                    }
+                },
+                    { },
+                    { })
+        )
+    }
+
+    private fun shouldStartLoginActivity() =
+        TokenCheckViewModel.shouldStartLogin.value?.let {
+            it && isTimeCountFinish
+        } ?: false
+
+    private fun shouldStartMainActivity() =
+        TokenCheckViewModel.shouldStartMain.value?.let {
+            it && isTimeCountFinish
+        } ?: false
 
     private fun startLoginActivity() {
         startActivity(LoginActivity::class.java)
@@ -46,22 +90,13 @@ class SplashActivity : AppCompatActivity() {
     }
 
     private fun <T> startActivity(activity: Class<T>) {
-        mDisposable = Single.just(activity)
-            .subscribeOn(Schedulers.io())
-            .observeOn(AndroidSchedulers.mainThread())
-            .subscribe(
-                {
-                    startActivity(Intent(this, it))
-                    overridePendingTransition(R.anim.enter_from_right, R.anim.exit_to_left)
-                    finish()
-                }, {
-                    it.printStackTrace()
-                }
-            )
+        startActivity(Intent(this, activity))
+        overridePendingTransition(R.anim.enter_from_right, R.anim.exit_to_left)
+        finish()
     }
 
     override fun onDestroy() {
-        mDisposable.dispose()
+        mCompositeDisposable.clear()
         TokenCheckViewModel.clearDisposable()
 
         super.onDestroy()
